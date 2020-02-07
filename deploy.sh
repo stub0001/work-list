213607ddcec37284ae69d93da8a8146ff30c2854
Bongjun Hur <bong@wiznet.io>
2020-02-07 14:48:33 +0900

    change baseurl and gh-pages branch again

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 set -e # exit with nonzero exit code if anything fails
 
-if [[ $TRAVIS_BRANCH == "code" && $TRAVIS_PULL_REQUEST == "false" ]]; then
+if [[ $TRAVIS_BRANCH == ${GH_SRC} && $TRAVIS_PULL_REQUEST == "false" ]]; then
 
 echo "Starting to update master\n"
 
@@ -15,10 +15,10 @@ git config --global user.email "bjnhur@gmail.com"
 git config --global user.name "bjnhur"
 
 #using token clone gh-pages branch
-git clone --quiet --branch=master https://${GH_TOKEN}@github.com/${GH_USER}/${GH_REPO}.git master > /dev/null
+git clone --quiet --branch=${GH_DST} https://${GH_TOKEN}@github.com/${GH_USER}/${GH_REPO}.git ${GH_DST} > /dev/null
 
 #go into directory and copy data we're interested in to that directory
-cd master
+cd ${GH_DST}
 cp -Rf $HOME/public/* .
 
 echo "Allow files with underscore https://help.github.com/articles/files-that-start-with-an-underscore-are-missing/" > .nojekyll
@@ -27,7 +27,7 @@ echo "[View live](https://${GH_USER}.github.io/${GH_REPO}/)" > README.md
 #add, commit and push files
 git add -f .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER"
-git push -fq origin master > /dev/null
+git push -fq origin ${GH_DST} > /dev/null
 
 echo "Done updating master\n"
 
